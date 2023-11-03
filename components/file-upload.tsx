fcd5bfe76b98d8794465652c4d4d897e01888efa
Dilang Lin <dilang@berkeley.edu>
2023-11-02 17:06:13 -0700

    change fileUrl to url

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -18,7 +18,7 @@ export const FileUpload = ( {
         <UploadDropzone 
             endpoint={endpoint}
             onClientUploadComplete={(res) => {
-                onChange(res?.[0].fileUrl);
+                onChange(res?.[0].url);
             }}
             onUploadError={(error: Error) => {
                 console.log(error);
