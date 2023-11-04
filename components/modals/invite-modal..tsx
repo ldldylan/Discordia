c49b16ba951dca4178bb6a0000c6380f18a3f495
Dilang Lin <dilang@berkeley.edu>
2023-11-03 20:02:42 -0700

    generate new url

--- components/modals/invite-modal..tsx
+++ components/modals/invite-modal..tsx
@@ -1,4 +1,6 @@
 "use client"
+import axios from "axios";
+
 import { Check, Copy, RefreshCw } from "lucide-react";
 
 import {
@@ -18,7 +20,7 @@ import { useState } from "react";
 
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type, data} = useModal();
+    const { onOpen, isOpen, onClose, type, data} = useModal();
     const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
@@ -34,6 +36,19 @@ export const InviteModal = () => {
         setTimeout(() => {
             setCopied(false);
         }, 1000);
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+            
+            onOpen("invite", { server: response.data })
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     const inviteUrl = `${origin}/invite/${server?.inviteCode}`
@@ -55,14 +70,17 @@ export const InviteModal = () => {
                     </Label>
                     <div className="flex items-center mt-2 gap-x-2">
                         <Input 
+                            disabled={isLoading}
                             className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                             value={inviteUrl}
                         />
-                        <Button onClick={onCopy} size="icon">
+                        <Button disabled={isLoading} onClick={onCopy} size="icon">
                             {copied ? <Check className="w-4 h-4" />: <Copy className="w-4 h-4"/>}
                         </Button>
                     </div>
                     <Button
+                        onClick={onNew}
+                        disabled={isLoading}
                         variant="link"
                         size="sm"
                         className="text-xs text-zinc-500 mt-4"
