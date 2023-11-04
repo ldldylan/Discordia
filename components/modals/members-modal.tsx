14ee2b72bad4da3fc49ebeac608a825c361117b9
Dilang Lin <dilang@berkeley.edu>
2023-11-03 23:50:03 -0700

    add onKick function

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -47,6 +47,27 @@ export const MembersModal = () => {
     const isModalOpen = isOpen && type === "members";
     const { server } = data as { server: ServerWithMembersWithProfiles};
     
+    const onKick = async (memberId: string) => {
+        try {
+            setLoadingId(memberId);
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id,
+                }
+            });
+
+            const response = await axios.delete(url);
+
+            router.refresh();
+            onOpen("members", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoadingId("");
+        }
+    }
+
     const onRoleChange = async (memberId: string, role: MemberRole) => {
         try {
             setLoadingId(memberId);
