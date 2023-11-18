d582db0ec6d7e17bb9a701caf4b3ce16327bbc8e
Dilang Lin <dilang@berkeley.edu>
2023-11-17 19:42:03 -0800

    add delete channel modal component

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,6 +6,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -25,6 +26,7 @@ export const ServerChannel = ({
     server,
     role
 }: ServerChannelProps) => {
+    const { onOpen } = useModal();
     const params = useParams();
     const router = useRouter();
 
@@ -54,6 +56,7 @@ export const ServerChannel = ({
                     </ActionTooltip>
                     <ActionTooltip label="Delete">
                         <Trash
+                            onClick={() => onOpen("deleteChannel", { server, channel })}
                             className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
                         />
                     </ActionTooltip>
